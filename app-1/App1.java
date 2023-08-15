65b5f56f1d8f6f76e76d46ce7b259bab2d651cf5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-15 17:10:34 +0530

    Fine tune the application

--- app-1/App1.java
+++ app-1/App1.java
@@ -9,9 +9,9 @@ public class App1 {
         final String COLOR_RED_BOLD = "\033[31;1m";
         final String RESET = "\033[0m";
 
-        final String DASHBOARD = "Welcome to SMS";
-        final String ADD_STUDENT = "Add New Student";
-        final String PRINT_DETAILS = "Print Student Details";
+        final String DASHBOARD = "👷 Welcome to SMS";
+        final String ADD_STUDENT = "➕ Add New Student";
+        final String PRINT_DETAILS = "🖨 Print Student Details";
 
         String[] students = new String[0];
         String screen = DASHBOARD;
@@ -21,44 +21,44 @@ public class App1 {
                                 COLOR_BLUE_BOLD, screen, RESET);
 
             System.out.println(CLEAR);
-            System.out.println("-".repeat(30));
-            System.out.println(" ".repeat((30 - APP_TITLE.length() + 7)/2).concat(APP_TITLE));
-            System.out.println("-".repeat(30));
+            // System.out.println("-".repeat(30));
+            System.out.println("\t" + APP_TITLE + "\n");
+            // System.out.println("-".repeat(30));
 
             switch(screen){
                 case DASHBOARD: 
-                    System.out.println("\n[1]. Add New Student");
-                    System.out.println("[2]. Print Student Details");
-                    System.out.println("[3]. Exit\n");
-                    System.out.print("Enter an option to continue > ");
+                    System.out.println("\t[1]. Add New Student");
+                    System.out.println("\t[2]. Print Student Details");
+                    System.out.println("\t[3]. Exit\n");
+                    System.out.print("\tEnter an option to continue: ");
                     int option = SCANNER.nextInt();
                     SCANNER.nextLine();
 
                     switch (option){
                         case 1: screen = ADD_STUDENT; break;
                         case 2: screen = PRINT_DETAILS; break;
-                        case 3: System.exit(0); break;
+                        case 3: System.out.println(CLEAR); System.exit(0);
                         default: continue;
                     }
                     break;
                 case ADD_STUDENT:
-                    System.out.printf("New Student ID: S%03d \n", (students.length + 1));
+                    System.out.printf("\tNew Student ID: S%03d \n", (students.length + 1));
 
                     boolean valid;
                     String name;
                     do{
                         valid = true;
-                        System.out.print("Enter Student Name: ");
+                        System.out.print("\tEnter Student Name: ");
                         name = SCANNER.nextLine().strip();
                         if (name.isBlank()){
-                            System.out.printf("%sName can't be empty%s\n", COLOR_RED_BOLD, RESET);
+                            System.out.printf("\t%sName can't be empty%s\n", COLOR_RED_BOLD, RESET);
                             valid = false;
                             continue;
                         }
                         for (int i = 0; i < name.length(); i++) {
                             if (!(Character.isLetter(name.charAt(i)) || 
                                 Character.isSpaceChar(name.charAt(i))) ) {
-                                System.out.printf("%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
+                                System.out.printf("\t%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
                                 valid = false;
                                 break;
                             }
@@ -73,34 +73,32 @@ public class App1 {
                     students = newStudents;
 
                     System.out.println();
-                    System.out.print(name + " added sucessfully. Do you want to add new student (Y/n)? ");
+                    System.out.print("\t" + name + " added sucessfully.\n\tDo you want to add new student (Y/n)? ");
                     if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) continue;
                     screen = DASHBOARD;
                     break;
                 case PRINT_DETAILS:
                     
                     if (students.length == 0){
-                        System.out.printf("%sNo students records found, please add a new student!%s \n",
+                        System.out.printf("\t%sNo students records found, please add a new student!%s \n",
                                     COLOR_RED_BOLD, RESET);                        
                     }else {
-                         final String LINE = "+" + "-".repeat(6) + "+" + "-".repeat(15) + "+";
+                         final String LINE = "\t+" + "-".repeat(6) + "+" + "-".repeat(15) + "+";
                          System.out.println(LINE);
                          // Header
-                         System.out.printf("|%-6s|%-15s|\n", "ID", "NAME");
+                         System.out.printf("\t|%-6s|%-15s|\n", "ID", "NAME");
                          System.out.println(LINE);
                          // Body
                          for (int i = 0; i < students.length; i++) {
                             String id = String.format("S%03d", (i+1));
-                            System.out.printf("|%-6s|%-15s|\n", id, students[i]);  
+                            System.out.printf("\t|%-6s|%-15s|\n", id, students[i]);  
                          }
                          System.out.println(LINE);
                     }
 
-                    System.out.print("Do you want to go back? (Y/n) ");
+                    System.out.print("\tDo you want to go back? (Y/n) ");
                     if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) screen = DASHBOARD;
                     break;
-                default:
-                    System.exit(0);
             }
             
         }while(true);
