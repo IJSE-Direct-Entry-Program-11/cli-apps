17083a1a849588439a39857d2eee6810023a1b91
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-15 18:58:09 +0530

    Finalize the app

--- app-2/App2.java
+++ app-2/App2.java
@@ -23,6 +23,7 @@ public class App2 {
 
         String screen = DASHBOARD;
 
+        mainLoop:
         do{
             final String APP_TITLE = String.format("%s%s%s",
             COLOR_BLUE_BOLD, screen, RESET);
@@ -121,28 +122,97 @@ public class App2 {
                     if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) continue;
                     screen = DASHBOARD;
                     break;
-                case REMOVE_CUSTOMER:
-                    // To-do
                 case PRINT_DETAILS:
-                if (customerIds.length == 0){
-                    System.out.printf(ERROR_MSG, "No customer records found, please add a new customer!");                      
-                }else {
-                     final String LINE = "\t+" + "-".repeat(6) + "+" + "-".repeat(15) + "+";
-                     System.out.println(LINE);
-                     // Header
-                     System.out.printf("\t|%-6s|%-15s|\n", "ID", "NAME");
-                     System.out.println(LINE);
-                     // Body
-                     for (int i = 0; i < customerIds.length; i++) {
-                        System.out.printf("\t|%-6s|%-15s|\n", customerIds[i],
-                         customerNames[i]);  
-                     }
-                     System.out.println(LINE);
-                }
+                    if (customerIds.length == 0){
+                        System.out.printf(ERROR_MSG, "No customer records found, please add a new customer!");                      
+                    }else {
+                        final String LINE = "\t+" + "-".repeat(6) + "+" + "-".repeat(15) + "+";
+                        System.out.println(LINE);
+                        // Header
+                        System.out.printf("\t|%-6s|%-15s|\n", "ID", "NAME");
+                        System.out.println(LINE);
+                        // Body
+                        for (int i = 0; i < customerIds.length; i++) {
+                            System.out.printf("\t|%-6s|%-15s|\n", customerIds[i],
+                            customerNames[i]);  
+                        }
+                        System.out.println(LINE);
+                    }
 
-                System.out.print("\tDo you want to go back? (Y/n) ");
-                if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) screen = DASHBOARD;
-                break;
+                    System.out.print("\tDo you want to go back? (Y/n) ");
+                    if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) screen = DASHBOARD;
+                    break;
+                case REMOVE_CUSTOMER:
+
+                    int index = 0;
+                    // ID Validation
+                    do {
+                        valid = true;
+                        System.out.print("\tEnter the Customer ID to delete: ");
+                        id = SCANNER.nextLine().toUpperCase().strip();
+                        if (id.isBlank()){
+                            System.out.printf(ERROR_MSG, "ID can't be empty");
+                            valid = false;
+                        }else if (!id.startsWith("C-") || id.length() < 3){
+                            System.out.printf(ERROR_MSG, "Invalid ID format");
+                            valid = false;
+                        }else{
+                            String number = id.substring(2);
+                            for (int i = 0; i < number.length(); i++) {
+                                if (!Character.isDigit(number.charAt(i))){
+                                    System.out.printf(ERROR_MSG, "Invalid ID format");
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                            boolean exists = false;
+                            for (int i = 0; i < customerIds.length; i++) {
+                                if (customerIds[i].equals(id)){
+                                    index = i;
+                                    exists = true;
+                                    break;
+                                }
+                            }    
+                            if (!exists){
+                                valid = false;
+                                System.out.printf(ERROR_MSG, "Customer ID does not exist");
+                            }
+                        }
+                        if (!valid) {
+                            System.out.print("\n\tDo you want to try again? (Y/n)");
+                            if (!SCANNER.nextLine().strip().toUpperCase().equals("Y")){
+                                screen = DASHBOARD;
+                                continue mainLoop;
+                            }
+                            System.out.println();
+                        }
+                    }while (!valid);
+
+                    newCustomerIds = new String[customerIds.length - 1];
+                    newCustomerNames = new String[newCustomerIds.length];
+
+                    for (int i = 0; i < customerIds.length; i++) {
+                        if (i < index){
+                            newCustomerIds[i] = customerIds[i];
+                            newCustomerNames[i] = customerNames[i];
+                        }else if (i == index){
+                            continue;
+                        }else{
+                            newCustomerIds[i - 1] = customerIds[i];
+                            newCustomerNames[i - 1] = customerNames[i];
+                        }
+                    }
+
+                    customerIds = newCustomerIds;
+                    customerNames = newCustomerNames;
+
+                    System.out.println();
+                    System.out.printf(SUCCESS_MSG, 
+                        String.format("%s has been deleted successfully", id));
+                    System.out.print("\tDo you want to continue adding (Y/n)? ");
+                    if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) continue;
+                    screen = DASHBOARD;
+                    break;
             }
         }while(true);
     }
