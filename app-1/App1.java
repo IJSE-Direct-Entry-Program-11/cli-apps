6c642a95e7b1729d35c6e98f35ea316002bf5667
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-15 16:21:21 +0530

    Add the add new student logic

--- app-1/App1.java
+++ app-1/App1.java
@@ -6,11 +6,14 @@ public class App1 {
     public static void main(String[] args) {
         final String CLEAR = "\033[H\033[2J";
         final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
         final String RESET = "\033[0m";
+
         final String DASHBOARD = "Welcome to SMS";
         final String ADD_STUDENT = "Add New Student";
-        final String REMOVE_STUDENT = "Remove Student";
         final String PRINT_DETAILS = "Print Student Details";
+
+        String[] students = new String[0];
         String screen = DASHBOARD;
 
         do {
@@ -25,25 +28,51 @@ public class App1 {
             switch(screen){
                 case DASHBOARD: 
                     System.out.println("\n[1]. Add New Student");
-                    System.out.println("[2]. Remove Existing Student");
-                    System.out.println("[3]. Print Student Details");
-                    System.out.println("[4]. Exit\n");
+                    System.out.println("[2]. Print Student Details");
+                    System.out.println("[3]. Exit\n");
                     System.out.print("Enter an option to continue > ");
                     int option = SCANNER.nextInt();
                     SCANNER.nextLine();
 
                     switch (option){
                         case 1: screen = ADD_STUDENT; break;
-                        case 2: screen = REMOVE_STUDENT; break;
-                        case 3: screen = PRINT_DETAILS; break;
-                        case 4: System.exit(0); break;
+                        case 2: screen = PRINT_DETAILS; break;
+                        case 3: System.exit(0); break;
                         default: continue;
                     }
                     break;
                 case ADD_STUDENT:
-                    System.out.println("New Student ID: S001");
-                    System.out.print("Enter Student Name: ");
-                    String name = SCANNER.nextLine();
+                    System.out.printf("New Student ID: S%03d \n", (students.length + 1));
+
+                    boolean valid;
+                    String name;
+                    do{
+                        valid = true;
+                        System.out.print("Enter Student Name: ");
+                        name = SCANNER.nextLine().strip();
+                        if (name.isBlank()){
+                            System.out.printf("%sName can't be empty%s\n", COLOR_RED_BOLD, RESET);
+                            valid = false;
+                            continue;
+                        }
+                        for (int i = 0; i < name.length(); i++) {
+                            if (!(Character.isLetter(name.charAt(i)) || 
+                                Character.isSpaceChar(name.charAt(i))) ) {
+                                System.out.printf("%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
+                                valid = false;
+                                break;
+                            }
+                        }
+                    }while(!valid);
+
+                    String[] newStudents = new String[students.length + 1];
+                    for (int i = 0; i < students.length; i++) {
+                        newStudents[i] = students[i];
+                    }
+                    newStudents[newStudents.length -1] = name;
+                    students = newStudents;
+
+                    System.out.println();
                     System.out.print(name + " added sucessfully. Do you want to add new student (Y/n)? ");
                     if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) continue;
                     screen = DASHBOARD;
